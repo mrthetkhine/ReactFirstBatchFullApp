2f664d757f9bed70f92cacefaf399aa0f3486542
Thet Khine <mrthetkhine@gmail.com>
2021-06-02 09:16:03 +0630

    Add Redux Dev Tools

--- react-redux-front-end/src/store/index.js
+++ react-redux-front-end/src/store/index.js
@@ -1,8 +1,8 @@
 import { createStore,applyMiddleware } from "redux";
 import thunk from 'redux-thunk';
-
+import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from "../reducers/index";
 
-const store = createStore(rootReducer,applyMiddleware(thunk));
-
+//const store = createStore(rootReducer,applyMiddleware(thunk));
+const store = createStore(rootReducer,composeWithDevTools( applyMiddleware(thunk)));
 export default store;
